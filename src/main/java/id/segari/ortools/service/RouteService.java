034dec0292ce6e115c592d4690cfcf0df89cfe1b
Mahfuz Jailani Ibrahim <mahfuzjailaniibrahim@gmail.com>
2026-01-05 12:04:42 +0700

    add controller

--- src/main/java/id/segari/ortools/service/RouteService.java
+++ src/main/java/id/segari/ortools/service/RouteService.java
@@ -4,6 +4,7 @@ import id.segari.ortools.dto.route.v1.RouteDTO;
 import id.segari.ortools.dto.route.v1.RouteResultDTO;
 import id.segari.ortools.dto.route.v2.RouteV2DTO;
 import id.segari.ortools.dto.route.v2.TspResultDTO;
+import id.segari.ortools.dto.route.v3.RouteV3DTO;
 import id.segari.ortools.validation.group.TspFixStartArbitraryFinish;
 import id.segari.ortools.validation.group.VrpArbitraryStartArbitraryFinish;
 import id.segari.ortools.validation.group.VrpSpStartArbitraryFinish;
@@ -21,4 +22,5 @@ public interface RouteService {
     RouteResultDTO tspWithFixStartAndArbitraryFinish(@Valid RouteDTO dto, @NotNull Integer index);
     RouteResultDTO tspWithSpStartAndArbitraryFinish(@Valid RouteDTO dto);
     TspResultDTO tspWithSpStartAndArbitraryFinishV2(@Valid RouteV2DTO dto);
+    TspResultDTO tspWithSpStartAndArbitraryFinishV3(@Valid RouteV3DTO dto);
 }
